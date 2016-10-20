3282125c2c7f478638de8b13830f662dfa5ba236
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2016-10-20 16:13:47 -0500

    Added some pseudo-code and more explanation as to the eventual structure of the two files in relation to one another

--- src/rvizViewer/src/findPoints.cpp
+++ src/rvizViewer/src/findPoints.cpp
@@ -1,4 +1,11 @@
 /*
     This .cpp file will be used to find all the points in the published .pcd object 
     model that are viewable from the pose passed in as a parameter
+
+
+    - Has no main
+    - filled with functions that are mainly private. The only public method will
+        be a method called from generate views and returns our quantification value
+        that indicates the percent of points on the object that are seen by that view
 */
+
--- src/rvizViewer/src/generateViews.cpp
+++ src/rvizViewer/src/generateViews.cpp
@@ -1,4 +1,33 @@
 /* 
     This .cpp file will be used to generate the different camera angles around the
     .pcd file. It will pass these views (poses) to the file findPoints.cpp
+
+
+    Program stucture:
+    
+    class{
+        .
+        .
+        .
+        .
+            Generates the views around the .pcd as poses
+        .
+        .
+        .
+        for(){
+            - goes through the list of poses as passes them over to
+                the findPoints.cpp
+            - A parrallel array will be made to hold the value this class will
+                return
+
+        }
+    
+        .
+        .
+        .
+        .
+        TBD
+
+    }
+
 */

c3aab0e4b3c5f02efa361c0405ca65e007917b0b
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2016-10-20 16:57:26 -0500

    created a drive .cpp file and updated to suggest an initial layout to the file

--- src/rvizViewer/src/findPoints.cpp
+++ src/rvizViewer/src/findPoints.cpp
@@ -1,11 +1,45 @@
 /*
-    This .cpp file will be used to find all the points in the published .pcd object 
-    model that are viewable from the pose passed in as a parameter
+   This .cpp file will be used to find all the points in the published .pcd object 
+   model that are viewable from the pose passed in as a parameter
+
+Attributes:
+
+- Has no main
+- filled with functions that are mainly private. The only public method will
+be a method called from generate views and returns our quantification value
+that indicates the percent of points on the object that are seen by that view
+
+To do:
+- need to subscirbe to the topic we are pushing the .pcd pointcloud to in
+the file rvizView
+
+ */
+
+#include <ros/ros.h>                                                                
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/publisher.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <ros/publisher.h>
+#include <string>
+//Used to include poses = views
+#include <geometry_msgs/Pose.msg>
 
 
-    - Has no main
-    - filled with functions that are mainly private. The only public method will
-        be a method called from generate views and returns our quantification value
-        that indicates the percent of points on the object that are seen by that view
-*/
+class findPoints(){
 
+    //Public function to get the pose from generateViews.cpp
+    public void quantifyDataInView(const  geometry_msgs::Pose &current_view)_{
+
+        //call functions
+        makeCone(*current_view);
+        //retirn vaue
+    }
+
+    private [type of cone] makeCone(pose){
+
+        //returns generated cone stucture
+    }
+
+}
---
+++ src/rvizViewer/src/runViewCreator.cpp
@@ -0,0 +1,10 @@
+/*
+    Driver class
+    
+
+    1. calls rvizView to publish .pcd
+    2. calls generateViews.cpp to create the poses
+        3. each pose makes a call to findPoints.cpp
+    4. 
+        TBD
+*/

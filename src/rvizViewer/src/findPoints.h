66ca7f51a99422dfefea829920b61e33894a3ef6
jkcooley <cooley.jk8@gmail.com>
2016-10-25 16:52:32 -0500

    Added header files and started to connect rvizView published .pcd with the subscriber findPoints.cpp

---
+++ src/rvizViewer/src/findPoints.h
@@ -0,0 +1,16 @@
+#ifndef FINDPOINTS_H
+#define FINDPOINTS_H
+
+#include <ros/ros.h>                                                                
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/publisher.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <ros/publisher.h>
+#include <string>
+#include <geometry_msgs/Pose.msg>
+
+public void quantifyDataInView(const geometry_msgs::Pose &current_view, /* pcd file*/) {
+
+}

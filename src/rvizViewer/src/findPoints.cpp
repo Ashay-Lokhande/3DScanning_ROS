ca2614e7c978905f36ad791b9c9ef3cfde3a6c20
Arnav Jain <archejain1021@gmail.com>
2016-11-01 01:14:45 -0500

    Points perception percentage

--- src/rvizViewer/src/findPoints.cpp
+++ src/rvizViewer/src/findPoints.cpp
@@ -1,67 +1,86 @@
-/*
-   This .cpp file will be used to find all the points in the published .pcd object 
-   model that are viewable from the pose passed in as a parameter
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/foreach.hpp>
+#include <math.h>
 
-Attributes:
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
-- Has no main
-- filled with functions that are mainly private. The only public method will
-be a method called from generate views and returns our quantification value
-that indicates the percent of points on the object that are seen by that view
+float calculate_slope(float pt_x, float pt_y, float pt_z, float pt2_x, float pt2_y, float pt2_z)
+{
+    float rise = pt2_z - pt_z;
 
-To do:
-- need to subscirbe to the topic we are pushing the .pcd pointcloud to in
-the file rvizView
+    // printf("Point 1: %f, %f. %f\n", pt_x, pt_y, pt_z);
+    // printf("Point 2: %f, %f, %f\n", pt2_x, pt2_y, pt2_z);
 
- */
+    float square = pow((pt2_x - pt_x), 2) + pow((pt2_y - pt_y), 2);
+    float run = sqrt(square);
+    // printf("What is the slope?: %f\n", rise/run);
 
-#include <findPoints.h>
+    return rise/run;
+}   
 
+bool onLine(float view_x, float view_y, float view_z, float pt_x, float pt_y, float pt_z, float check_pt_x, float check_pt_y, float check_pt_z)
+{
+    // Calculate the slope from the view to the check_pt
+    float slope_pt_to_view = calculate_slope(view_x, view_y, view_z, check_pt_x, check_pt_y, check_pt_z);
 
-class findPoints(){
+    // printf("Slope from view to check_pt: %f\n", slope_pt_to_view);
 
-    //Public function to get the pose from generateViews.cpp
-    public void quantifyDataInView(const geometry_msgs::Pose &current_view)_{
+    // Calculate slope from the check_pt to the pt
+    float slope_check_pt_to_pt = calculate_slope(view_x, view_y, view_z, pt_x, pt_y, pt_z);
 
-    	//subscibes to the cloud topic containing the pointcloud of the object we are scanning
-    	// MAY HAVE TO CHANGE /cloud to cloud
-    	ros::Subscriber sub = n.subscribe("/cloud", 1000, cloudCallback);
+    // printf("Slope from view to pt: %f\n", slope_check_pt_to_pt);
+    // printf("Are they equivalent?: %d\n", temp);
 
-        //call functions
-        	//type cone cone = makeCone(*current_view);
-			/*
-			Looping structure that generates lines from the pose and within the cone's width view
-				loop:
-					contains a total value to count the number of lines that hit a point on the object
-					calls function and passes the line, function checks weather or not it hits the object we are scanning
-			*/
+    float delta = 0.0000045f;
 
-        //retirn vaue whihc will be the percent (lines that hit / lines generated)
+    float diff = fabs(slope_pt_to_view - slope_check_pt_to_pt);
+
+    bool temp = (diff < delta);
+    // printf("Difference: %f\n", diff);
+    // printf("Boolean: %d\n", temp);
+
+    //printf("Difference: %f\n", diff);
+    // If they are equal return true
+    return temp;
+}
+
+void callback(const PointCloud::ConstPtr& msg)
+{
+    // printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
+    // Coordinates of the view pose relative to the center of the object
+    float x = 5.0;
+    float y = 5.0;
+    float z = 5.0;
+    int count = 0;
+    int size = 0;
+    // Compare for each point in the pcd
+    BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points) {
+        int numberInLine = 0;
+        BOOST_FOREACH (const pcl::PointXYZ& pt2, msg->points) {
+            bool onTheLine = onLine(x, y, z, pt.x, pt.y, pt.z, pt2.x, pt2.y, pt2.z);
+            if (onTheLine) {
+                numberInLine++;
+            }
+        }
+        // printf("inView: %d\n", inView);
+        if (numberInLine <= 1) {
+            count++;
+        }
+        size++;
     }
 
+    // printf("Count: %d\n", count);
+    // printf("Size: %d\n", size);
 
-    // in preivous method we subscribe to the same topic 
-    // to which the pcd object has been published to
-    // we subscribe to that topic "/cloud" and expect the callBackFunction to get passed
-    // the pcd object
-    void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& pcdObject){
+    printf("Percent of points viewed from pose: %f\n", 100 * count/(float)size);
+}
 
-    	// we get the pcd object -- like the spray bottle, etc
-
-
-  		
-	}
-
-    private [type of cone] makeCone(pose){
-
-        //returns generated cone stucture at the pise
-    }
-
-    private boolean validLine(/*The line object, the .pcd object*/){
-
-    	//checks to see if the line hits a point on the object 
-    	//returns t/f depending on if it hits or not
-    }
-
-
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "generate_views");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe<PointCloud>("cloud", 1, callback);
+    ros::spin();
 }

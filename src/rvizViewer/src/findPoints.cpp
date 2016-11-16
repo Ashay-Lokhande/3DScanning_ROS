99a388c74aecd1728d8569b8958406fccd1478cd
Brahma Pavse <brahmasp@utexas.edu>
2016-11-16 17:09:54 -0600

    New: closest viewable points collected in map impl - angles pending - findpoints.cpp

--- src/rvizViewer/src/findPoints.cpp
+++ src/rvizViewer/src/findPoints.cpp
@@ -4,48 +4,60 @@
 #include <boost/foreach.hpp>
 #include <math.h>
 #include <geometry_msgs/Pose.h>
-// #include <unordered_set>
+#include <set>
+#include <map>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
+
+// represnts a point with x,y,z coordinates
+struct Point {
+
+    float x;
+    float y;
+    float z;
+};
+
+// calculating slope of line formed by two points in 3d space
 float calculate_slope(float pt_x, float pt_y, float pt_z, float pt2_x, float pt2_y, float pt2_z)
 {
     float rise = pt2_z - pt_z;
 
-    // printf("Point 1: %f, %f. %f\n", pt_x, pt_y, pt_z);
-    // printf("Point 2: %f, %f, %f\n", pt2_x, pt2_y, pt2_z);
-
     float square = pow((pt2_x - pt_x), 2) + pow((pt2_y - pt_y), 2);
     float run = sqrt(square);
-    // printf("What is the slope?: %f\n", rise/run);
-
     return rise/run;
 }   
 
-bool onLine(float view_x, float view_y, float view_z, float pt_x, float pt_y, float pt_z, float check_pt_x, float check_pt_y, float check_pt_z)
-{
-    // Calculate the slope from the view to the check_pt
-    float slope_pt_to_view = calculate_slope(view_x, view_y, view_z, check_pt_x, check_pt_y, check_pt_z);
+// bool onLine(float view_x, float view_y, float view_z, float pt_x, float pt_y, float pt_z, float check_pt_x, float check_pt_y, float check_pt_z)
+// {
+//     // Calculate the slope from the view to the check_pt
+//     float slope_pt_to_view = calculate_slope(view_x, view_y, view_z, check_pt_x, check_pt_y, check_pt_z);
 
-    // printf("Slope from view to check_pt: %f\n", slope_pt_to_view);
+//     // printf("Slope from view to check_pt: %f\n", slope_pt_to_view);
 
-    // Calculate slope from the check_pt to the pt
-    float slope_check_pt_to_pt = calculate_slope(view_x, view_y, view_z, pt_x, pt_y, pt_z);
+//     // Calculate slope from the check_pt to the pt
+//     float slope_check_pt_to_pt = calculate_slope(view_x, view_y, view_z, pt_x, pt_y, pt_z);
 
-    // printf("Slope from view to pt: %f\n", slope_check_pt_to_pt);
-    // printf("Are they equivalent?: %d\n", temp);
+//     // printf("Slope from view to pt: %f\n", slope_check_pt_to_pt);
+//     // printf("Are they equivalent?: %d\n", temp);
 
-    float delta = 0.0000000045f;
+//     float delta = 0.0000000045f;
 
-    float diff = fabs(slope_pt_to_view - slope_check_pt_to_pt);
+//     float diff = fabs(slope_pt_to_view - slope_check_pt_to_pt);
 
-    bool temp = (diff < delta);
-    // printf("Difference: %f\n", diff);
-    // printf("Boolean: %d\n", temp);
+//     bool temp = (diff < delta);
+//     // printf("Difference: %f\n", diff);
+//     // printf("Boolean: %d\n", temp);
 
-    //printf("Difference: %f\n", diff);
-    // If they are equal return true
-    return temp;
+//     //printf("Difference: %f\n", diff);
+//     // If they are equal return true
+//     return temp;
+// }
+
+// calculating distance between two points in  3d space
+float distance(float x1, float y1, float z1, float x2, float y2, float z2){
+
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2));
 }
 
 
@@ -53,43 +65,73 @@ bool onLine(float view_x, float view_y, float view_z, float pt_x, float pt_y, fl
 // arg1: Passed in a pose that represents a point in space
 // arg2: the point cloud object of the pcd file
 
-// PEDNING: return the actual viewable points instead of just the percentage
 // PENDING: consider orientation (various angles for a given point) when viewing the object
 float findPoints(const geometry_msgs::Pose createdPoint, const PointCloud::ConstPtr& msg)
 {
 
 
-	// printing the current view point
+    // printing the current view point
     printf("Coordinates: %f, %f, %f\n", createdPoint.position.x, createdPoint.position.y, createdPoint.position.z);
-    
-    // build a hashset and put the slopes in here
-    //std::unordered_set<float> seenSlopes;
 
     // x,y,z represents the cooridnates of the view 
     float x = createdPoint.position.x;
     float y = createdPoint.position.y;
     float z = createdPoint.position.z;
 
-    // count of viewable points
-    int count = 0;
-
     // total number of points in the point cloud
     int size = 0;
 
-    // PENDING!!
     // idea is that for a given line from the view point, slope is the same
     // we want to count for a single point and exclude all the other points on that line
     // by that way we count for a single point and not count points behind the counted 
     // point
-
     // if slope is already contained, then check which point is closer
     // to the view point
-    // BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points) {
-    // 	float slope_pt_to_view = calculate_slope(x, y, z, pt.x, pt.y, pt.z);
-    // 	seenSlopes.insert(slope_pt_to_view); // should eradicate all duplicates
-    // 	size++; // counting total number of points viewed
+    // map for: slope -> point
+    std::map<float, Point> viewablePoints;
+    std::map<float, Point>::iterator it;
+    BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points) {
+        float slope_pt_to_view = (int) floorf(calculate_slope(x, y, z, pt.x, pt.y, pt.z) * 100000000.0 + 0.5)/100000000.0;
+        it = viewablePoints.find(slope_pt_to_view);
+
+        // if slope is already present
+        if(it != viewablePoints.end()){
+            // if the slop is already taken into account
+            // then we must see the point it corresponds to is the closest to the view point
+            // if this new point is closer then must updates the map accordingly
+            Point existingPoint = it->second; // get the point
+            float oldDistance = distance(x, y, z, existingPoint.x, existingPoint.y, existingPoint.z);
+            float newDistance = distance(x, y, z, pt.x, pt.y, pt.z);
+            if(newDistance < oldDistance) {
+                Point newPoint;
+                newPoint.x = pt.x;
+                newPoint.y = pt.y;
+                newPoint.z = pt.z;
+                viewablePoints.insert(std::pair<float, Point> (slope_pt_to_view, newPoint));
+                //printf("Old point: %f, %f, %f with distance of %f\n", existingPoint.x, existingPoint.y, existingPoint.z, oldDistance);
+                //printf("New point: %f, %f, %f with distance of %f\n", newPoint.x, newPoint.y, newPoint.z, newDistance);
+            }
+
+        } else {
+            // first time looking at this point
+            // just add it
+            Point p;
+            p.x = pt.x;
+            p.y = pt.y;
+            p.z = pt.z;
+            viewablePoints.insert(std::pair<float, Point> (slope_pt_to_view, p));
+        }
+        size++; // totalNum points
+    }
+
+    // displaying contents of the map
+    // multipying by 10000 because %f truncates values, so it appears as if duplictes exist
+    // for(it = viewablePoints.begin(); it != viewablePoints.end(); it++){
+    //     printf("key: %f, Value: %f, %f, %f\n", it->first * 10000, it->second.x, it->second.y, it->second.z);
     // }
-    // return (100 * seenSlopes.size() / (float) (size)); // percentag of points viewed
+    float viewableAmount = 100 * viewablePoints.size() / (float) (size);
+    return viewableAmount; // percentag of points viewed
+
 
 
 
@@ -101,28 +143,29 @@ float findPoints(const geometry_msgs::Pose createdPoint, const PointCloud::Const
     // if they are similar we discount this because we have seen this point before
     // by this way we count all the points, without duplicates
     // by doing numberInLine <=1 we make sure we count only unique points and not duplicates
-    BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points) {
-        int numberInLine = 0;
-        BOOST_FOREACH (const pcl::PointXYZ& pt2, msg->points) {
-            bool onTheLine = onLine(x, y, z, pt.x, pt.y, pt.z, pt2.x, pt2.y, pt2.z);
-            if (onTheLine) {
-                numberInLine++;
-            }
-        }
-        // printf("inView: %d\n", inView);
-        if (numberInLine <= 1) {
-            count++;
-        }
-        size++;
-    }
+    // BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points) {
+    //     int numberInLine = 0;
+    //     BOOST_FOREACH (const pcl::PointXYZ& pt2, msg->points) {
+    //         bool onTheLine = onLine(x, y, z, pt.x, pt.y, pt.z, pt2.x, pt2.y, pt2.z);
+    //         if (onTheLine) {
+    //             numberInLine++;
+    //         }
+    //     }
+    //     // printf("inView: %d\n", inView);
+    //     if (numberInLine <= 1) {
+    //         count++;
+    //     }
+    //     size++;
+    // }
 
-    // printf("Count: %d\n", count);
-    // printf("Size: %d\n", size);
-    //printf("Percent of points viewed from pose: %f\n", 100 * count/(float)size);
+    // // printf("Count: %d\n", count);
+    // // printf("Size: %d\n", size);
 
-    // finally return a ratio of the points seen to the total number of points 
-    // in the pcl 
-    return (100 * count / (float) (size));
+    // //printf("Percent of points viewed from pose: %f\n", 100 * count/(float)size);
+
+    // // finally return a ratio of the points seen to the total number of points 
+    // // in the pcl 
+    // return (100 * count / (float) (size));
 }
 
 // void callback(const PointCloud::ConstPtr& msg)
